8d9028e744c38878a645f0fde342f62ea7459ea3
中河雅 <nakagawamasashi@MacBook-Pro.local>
2023-07-29 15:01:02 +0900

    renderの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = '150455ac019970c0d98f';
-const GITHUB_CLIENT_SECRET = 'a96ed022e304315539e8573e4f44959a60eee58a';
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '150455ac019970c0d98f';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'a96ed022e304315539e8573e4f44959a60eee58a';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -39,7 +39,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.CALLBACK_URL || 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(async function () {
